63dfd224c8d22b38822cde30663711091968a41a
phaldaros <derikrichards+phaldarosgithub@gmail.com>
2020-08-26 15:32:36 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+This is a Foundry VTT system for Ars Magica 5th Edition.
+
+This package is intended to enable Ars Magica play using Foundry VTT. In order to play, official Ars Magica materials are still required.
+
+Ars Magica content is copyright Trident, Inc d/b/a Atlas Games. Ars Magica, Mythic Europe, and Covenants are trademarks of Trident, Inc. Order of Hermes and Tremere are trademarks of White Wolf, Inc.
+
+Official material for Ars Magica 5th edition is published by and may be purchased from Atlas Games (https://atlas-games.com).
+
+This project was based on the [Boilerplate System](https://gitlab.com/asacolips-projects/foundry-mods/boilerplate) by Asacolips.
+
+TO DO:
+- implement the non-Magus actor types (companion, grog, covenant)
+- fix warp, decrepitude, reputation
+- abilities requiring specializations
+- ability net cost/point spend
+- "+" buttons
+- add books item type for summae/tractati
+- find a better Lombardi font
+- deal with encumbrance/load/"weight"
+- implement stress die / botch die / exploding mechanic
---
+++ module/arm5e.js
@@ -0,0 +1,103 @@
+// Import Modules
+import { ArM5eActor } from "./actor/actor.js";
+import { ArM5eActorSheet } from "./actor/actor-sheet.js";
+import { ArM5eItem } from "./item/item.js";
+import { ArM5eItemSheet } from "./item/item-sheet.js";
+
+Hooks.once('init', async function() {
+
+  game.arm5e = {
+    ArM5eActor,
+    ArM5eItem,
+    rollItemMacro
+  };
+
+  /**
+   * Set an initiative formula for the system
+   * @type {String}
+   */
+  CONFIG.Combat.initiative = {
+    formula: "1d10 + @characteristics.qik.value",
+    decimals: 2
+  };
+
+  // Define custom Entity classes
+  CONFIG.Actor.entityClass = ArM5eActor;
+  CONFIG.Item.entityClass = ArM5eItem;
+
+  // Register sheet application classes
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("arm5e", ArM5eActorSheet, { makeDefault: true });
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("arm5e", ArM5eItemSheet, { makeDefault: true });
+
+  // If you need to add Handlebars helpers, here are a few useful examples:
+  Handlebars.registerHelper('concat', function() {
+    var outStr = '';
+    for (var arg in arguments) {
+      if (typeof arguments[arg] != 'object') {
+        outStr += arguments[arg];
+      }
+    }
+    return outStr;
+  });
+
+  Handlebars.registerHelper('toLowerCase', function(str) {
+    return str.toLowerCase();
+  });
+});
+
+Hooks.once("ready", async function() {
+  // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
+  Hooks.on("hotbarDrop", (bar, data, slot) => createArM5eMacro(data, slot));
+});
+
+/* -------------------------------------------- */
+/*  Hotbar Macros                               */
+/* -------------------------------------------- */
+
+/**
+ * Create a Macro from an Item drop.
+ * Get an existing item macro if one exists, otherwise create a new one.
+ * @param {Object} data     The dropped data
+ * @param {number} slot     The hotbar slot to use
+ * @returns {Promise}
+ */
+async function createArM5eMacro(data, slot) {
+  if (data.type !== "Item") return;
+  if (!("data" in data)) return ui.notifications.warn("You can only create macro buttons for owned Items");
+  const item = data.data;
+
+  // Create the macro command
+  const command = `game.arm5e.rollItemMacro("${item.name}");`;
+  let macro = game.macros.entities.find(m => (m.name === item.name) && (m.command === command));
+  if (!macro) {
+    macro = await Macro.create({
+      name: item.name,
+      type: "script",
+      img: item.img,
+      command: command,
+      flags: { "arm5e.itemMacro": true }
+    });
+  }
+  game.user.assignHotbarMacro(macro, slot);
+  return false;
+}
+
+/**
+ * Create a Macro from an Item drop.
+ * Get an existing item macro if one exists, otherwise create a new one.
+ * @param {string} itemName
+ * @return {Promise}
+ */
+function rollItemMacro(itemName) {
+  const speaker = ChatMessage.getSpeaker();
+  let actor;
+  if (speaker.token) actor = game.actors.tokens[speaker.token];
+  if (!actor) actor = game.actors.get(speaker.actor);
+  const item = actor ? actor.items.find(i => i.name === itemName) : null;
+  if (!item) return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemName}`);
+
+  // Trigger the item roll
+  return item.roll();
+}
