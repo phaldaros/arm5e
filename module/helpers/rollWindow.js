dc2a3720b611d4f12c2968b9bc4f865d168980e7
LudoBermejoES <LudoGithub@gmail.com>
2022-02-01 08:21:47 +0000

    BUGFIX: Spontaneous magic and magical effects are affected now by active effects related with spellcasting

--- module/helpers/rollWindow.js
+++ module/helpers/rollWindow.js
@@ -106,18 +106,16 @@ function prepareRollVariables(dataset, actorData, activeEffects) {
 
       if (dataset.bonusActiveEffects) {
         actorData.data.roll.bonusActiveEffects = Number(dataset.bonusActiveEffects);
-        if (dataset.roll === "spell") {
-          const activeEffectsByType = findAllActiveEffectsByType(activeEffects, ACTIVE_EFFECTS_TYPES.SPELLCASTING.type);
-          actorData.data.roll.activeEffects = activeEffectsByType.map((activeEffect) => {
-            const label = activeEffect.data.label;
-            let value = 0;
-            activeEffect.data.changes.forEach((item) => (value += Number(item.value)));
-            return {
-              label,
-              value
-            };
-          });
-        }
+        const activeEffectsByType = findAllActiveEffectsByType(activeEffects, ACTIVE_EFFECTS_TYPES.SPELLCASTING.type);
+        actorData.data.roll.activeEffects = activeEffectsByType.map((activeEffect) => {
+          const label = activeEffect.data.label;
+          let value = 0;
+          activeEffect.data.changes.forEach((item) => (value += Number(item.value)));
+          return {
+            label,
+            value
+          };
+        });
       }
 
       if (dataset.technique) {
