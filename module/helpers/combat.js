eb44a8cbcc26029b2c802a6a8e3099915357ba22
Xzotl <xzotl42@gmail.com>
2022-06-04 12:16:12 +0200

    Combat exertion

---
+++ module/helpers/combat.js
@@ -0,0 +1,12 @@
+import { log } from "../tools.js";
+import { stressDie } from "../dice.js";
+
+export async function exertSelf(html, actor, mode, callback, roll) {
+  log(false, "Exert self in combat");
+
+  actor.data.data.roll.exertion = true;
+
+  await stressDie(html, actor, mode, callback, roll);
+
+  actor.loseFatigueLevel(1);
+}
